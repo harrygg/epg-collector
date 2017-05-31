ba05aeb272dbdf9c474e66f8db551ad36d95c87f
Harry G <harrygg@gmail.com>
2017-05-31 10:12:37 +0300

    fixed ms days

--- ms.py
+++ ms.py
@@ -14,57 +14,45 @@ from helper import *
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
-days = []
-now = datetime.datetime.now()
-days.append(now.strftime('%d'))
-today = now.strftime('%Y%m%d')
-tomorrow = now + datetime.timedelta(days=1)
-days.append(tomorrow.strftime('%d'))
-thedayafter = now + datetime.timedelta(days=2)
-days.append(thedayafter.strftime('%d'))
-afterthedayafter = now + datetime.timedelta(days=3)
-days.append(afterthedayafter.strftime('%d'))
-
-epoch_times = []
-epoch_today = str(time.mktime(now.timetuple()))[:10]
-epoch_times.append(epoch_today)
-epoch_tomorrow = str(time.mktime(tomorrow.timetuple()))[:10]
-epoch_times.append(epoch_tomorrow)
-epoch_thedayafter = str(time.mktime(thedayafter.timetuple()))[:10]
-epoch_times.append(epoch_thedayafter)
-epoch_afterthedayafter = str(time.mktime(afterthedayafter.timetuple()))[:10]
-epoch_times.append(epoch_afterthedayafter)
-
-start_epoch = str(time.mktime(now.timetuple()))[:10]
-end_epoch = str(time.mktime(thedayafter.timetuple()))[:10]
-#epoch = datetime.datetime.utcfromtimestamp(0)
-
+MAXDAYS = 3
 host = base64.b64decode("aHR0cDovL21vdmllc3Rhci5iZy8=")
 headers = {"User-agent":"Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36" , "Referer": host}
 url_template = "%s?rhc_action=get_calendar_events&post_type[]=events&start=%s&end=%s&rhc_shrink=0&view=agendaDay"
 
+### Calculate dates for for scrabbing days
+dates = {"days":[], "datetimes":[], "epochtimes":[]}
+now = datetime.datetime.now()
+end = MAXDAYS + 1 # get an extra day epoch time as a limit for previous day
+for i in range(0, end): 
+  current_day = now + datetime.timedelta(days=i)
+  dates["days"].append(current_day.strftime('%d'))
+  dates["datetimes"].append(current_day.strftime('%Y%m%d'))
+  #add epoch time i.e. 1496213915
+  dates["epochtimes"].append(str(time.mktime(current_day.timetuple()))[:10])
+
+if not os.path.exists("moviestar"):
+  os.makedirs("moviestar")
+    
 ### Iterate days
-for i in range(0, 3):
-  url = url_template % (host, epoch_times[i], epoch_times[i+1])
+for i in range(0, MAXDAYS):
+  url = url_template % (host, dates["epochtimes"][i], dates["epochtimes"][i+1])
   print "Getting url: %s " % url
   r = requests.get(url, headers=headers)
   print "Server response: %s " % r.status_code
 
-  if not os.path.exists("moviestar"):
-    os.makedirs("moviestar")
-    
-  file_name = "moviestar/%s.json" % days[i]
+  file_name = "moviestar/%s.json" % dates["days"][i]
   with open(file_name, "w") as f:
     f.write(r.text[3:].encode("utf-8"))
 
   items = json.load(codecs.open(file_name, 'r', 'utf-8-sig'))
   
   programs = []
+  programs_sorted = []
   
   for item in items["EVENTS"]:
     rdates = item["fc_rdate"].split(",")
     for rdate in rdates:
-      if today in rdate or tomorrow.strftime('%Y%m%d') in rdate:
+      if dates["datetimes"][i] in rdate:
         
         title = normalize(item["title"])        
         starttime = rdate[9:11]+":"+rdate[11:13]
@@ -75,10 +63,10 @@ for i in range(0, 3):
         programs.append(program)
   
   print "Sorting"
-  programs_sorted = sorted(programs, key=lambda m: m.datetime, reverse=False)
+  programs_sorted = sorted(programs, key=lambda p: p.datetime, reverse=False)
   
-  get_movie_details = True
-  if (get_movie_details):
+  debug = True
+  if not debug:
     print "Searching for movie details"
     programs = []
     for program in programs_sorted:
@@ -131,5 +119,6 @@ for i in range(0, 3):
       del program.datetime
       programs.append(program.__dict__)
   
-  with open(file_name, "w") as f:
-    f.write(pretty_json(programs))
+  if not debug:
+    with open(file_name, "w") as f:
+      f.write(pretty_json(programs))
